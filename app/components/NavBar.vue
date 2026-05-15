f2cc66da6b96ca8e07416046399eca41a8d07425
x <x>
2026-05-15 09:51:40 +0300

    new

--- app/components/NavBar.vue
+++ app/components/NavBar.vue
@@ -1,23 +1,25 @@
 <script setup lang="tsx">
   const animate = ref(false);
 
-  onMounted(() => animate.value = true);
+  onMounted(() => (animate.value = true));
 </script>
 
 <template>
   <div
-    class="absolute top-0 flex w-full z-20 items-center justify-center font-[Inter]"
+    class="absolute top-0 z-20 flex w-full items-center justify-center font-[Inter]"
   >
     <div
       class="flex h-full w-full max-w-467.5 items-center justify-between p-3 md:p-5 md:px-8"
     >
       <NuxtLink
         to="/"
-        class="flex transition-all duration-800 items-center gap-2 md:gap-3"
-        :class="animate ? 'translate-x-0 opacity-100' : 'opacity-0 -translate-x-10'"
+        class="flex items-center gap-2 transition-all duration-800 md:gap-3"
+        :class="
+          animate ? 'translate-x-0 opacity-100' : '-translate-x-10 opacity-0'
+        "
       >
         <div
-          class="flex flex-col justify-center font-[FontSpring] hover:scale-106 transition-all font-bold text-lg md:text-2xl"
+          class="flex flex-col justify-center font-[FontSpring] text-lg font-bold transition-all hover:scale-106 md:text-2xl"
         >
           <div>ALYA</div>
           <!-- <div>Construction</div> -->
@@ -25,8 +27,10 @@
       </NuxtLink>
 
       <div
-        :class="animate ? 'translate-y-0 opacity-100' : 'opacity-0 -translate-y-10'"
-        class="flex flex-1 items-center transition-all duration-500 justify-center text-gray-900"
+        :class="
+          animate ? 'translate-y-0 opacity-100' : '-translate-y-10 opacity-0'
+        "
+        class="flex flex-1 items-center justify-center text-gray-900 transition-all duration-500"
       >
         <NavLinks />
       </div>
@@ -36,8 +40,10 @@
       </div>
 
       <ActionBtn
-        :class="animate ? 'translate-x-0 opacity-100' : 'opacity-0 translate-x-10'"
-        class="transition-all text-nowrap duration-500 hover:scale-101 p-2 px-2 text-xs md:px-4 md:text-sm bg-black text-white"
+        :class="
+          animate ? 'translate-x-0 opacity-100' : 'translate-x-10 opacity-0'
+        "
+        class="bg-black p-2 px-2 text-xs text-nowrap text-white transition-all duration-500 hover:scale-101 md:px-4 md:text-sm"
         :call-back="() => {}"
       >
         Book A Call
