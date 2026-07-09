c1ff4e02cb792e4adda3be6226b52c6362e50d43
x <x>
2026-07-10 00:12:07 +0300

    fixes

--- app/pages/about.vue
+++ app/pages/about.vue
@@ -110,7 +110,7 @@
         </div>
       </div>
       <div
-        class="group relative h-max /aspect-3/4 /w-full overflow-hidden rounded-2xl lg:w-1/2"
+        class="group relative h-max /aspect-3/4 w-full overflow-hidden rounded-2xl lg:w-1/2"
       >
         <img
           src="/section.jpg"
