5d1480bf598c5af6cba984e4f9e9270ff2dcc298
x <x>
2026-07-12 11:09:27 +0300

    fix

--- app/pages/projects_/[id].vue
+++ app/pages/projects_/[id].vue
@@ -87,7 +87,7 @@
         <NuxtImg
           :src="projectImage"
 			:placeholder="1"
-          class="h-full max-h-[50svh] w-auto object-cover transition-all duration-1000"
+          class="h-full max-h-[57svh] w-auto object-cover transition-all duration-1000"
         />
       </div>
     </div>
