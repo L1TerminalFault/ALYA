2010c9379611bbed9b8b23f705bd82f92df71708
x <x>
2026-07-12 08:51:09 +0300

    fix

--- app/pages/gallery.vue
+++ app/pages/gallery.vue
@@ -87,13 +87,13 @@
     <!-- Image Modal -->
     <div
       v-if="selectedImage"
-      class="fixed max-h-[80vh] max-w-[80vw] min-h-[60svh] min-w-[60svw] rounded-lg inset-0 z-100 flex cursor-zoom-out items-center justify-center bg-gray-950/90 p-5 backdrop-blur-sm"
+      class="fixed inset-0 z-100 flex cursor-zoom-out items-center justify-center bg-gray-950/90 p-5 backdrop-blur-sm"
       @click="closeModal"
     >
       <NuxtImg
         :src="selectedImage"
         :placeholder="1"
-        class="inset-0 w-full h-full rounded-lg object-contain shadow-2xl"
+        class="inset-0 max-h-[80vh] max-w-[80vw] min-h-[60svh] min-w-[60svw] w-full h-full overflow-hidden rounded-lg object-contain shadow-2xl"
         @click.stop
       />
 
